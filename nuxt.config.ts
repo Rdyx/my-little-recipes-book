9786b899000f957b467ccc73932ec1d1e21d00a7
Nicolas T <tiranico.pro@gmail.com>
2024-01-07 02:37:00 +0100

    Create nuxtjs.yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: { prerender: { failOnError: false } },
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/test-utils/module', '@vite-pwa/nuxt'],
   typescript: {
-    typeCheck: true,
-    strict: true
+    // typeCheck: true,
+    // strict: true
   },
   pwa: {
     devOptions: {
       enabled: true
     }
-  }
+  },
+  ssr: false
+  // nitro: { prerender: { failOnError: false } }
 })
